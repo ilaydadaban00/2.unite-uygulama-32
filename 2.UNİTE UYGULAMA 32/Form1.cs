23a6514dffa57241841d88acd93dc046aea6988f
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-03-09 10:45:25 +0300

    Add files via upload

---
+++ 2.UNİTE UYGULAMA 32/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.UNİTE_UYGULAMA_32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int SAYİ1, KARESİ;
+            try
+            {
+                SAYİ1 = Convert.ToInt16(textBox1.Text);
+                KARESİ = SAYİ1 * SAYİ1;
+                MessageBox.Show("sayının karesi=    " + KARESİ);
+            }
+            catch
+            {
+                MessageBox.Show("hatatlı   ");
+            }
+         
+        }
+    }
+}
